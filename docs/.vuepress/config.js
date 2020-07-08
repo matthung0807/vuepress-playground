f8d6d1f28adb425978a993684548e944802189f1
Matt Hung <matthung@cherricorp.com>
2020-07-08 16:23:44 +0800

    add config.js with navbar config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'VuePress', link: 'https://vuepress.vuejs.org/' }
+    ]
+  }
+}
